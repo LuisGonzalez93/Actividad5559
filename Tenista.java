cccef335db7c34ea5fd1982b5200374c0e0cdd5d
Héctor García García <Hectoruno>
2019-02-06 12:24:49 +0100

    Commit 01.

---
+++ Tenista.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Tenista here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tenista
+{
+    // instance variables - replace the example below with your own
+    private String nombre;
+    private int anoNacimiento;
+    private boolean diestro;
+    private double peso;
+
+    /**
+     * Constructor for objects of class Tenista
+     */
+    public Tenista(String nombre, int anoNacimiento, boolean diestro, double peso)
+    {
+        this.nombre = nombre;
+        this.anoNacimiento = anoNacimiento;
+        this.diestro = diestro;
+        this.peso = peso;
+    }
+}
